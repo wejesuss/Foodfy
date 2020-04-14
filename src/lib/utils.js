d4b01f944467ce9ea47f490ad59af8003f5f536e
Wemerson Jesus <wejesuss@gmail.com>
2020-04-14 17:39:23 -0300

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,4 @@
 module.exports = {
-    date(timestamp) {
-        const birthDate = new Date(timestamp)
-
-        const year = birthDate.getFullYear()
-        const month = `0${birthDate.getMonth() + 1}`.slice(-2)
-        const day = `0${birthDate.getDate()}`.slice(-2)
-        
-        return {
-            iso:`${year}-${month}-${day}`,
-            birthDay:`${day}/${month}`,
-            format:`${day}/${month}/${year}`
-        }
-    },
     addSrcToFilesArray(files) {
         let filesWithSrc = files.map(file => ({ 
             ...file,
